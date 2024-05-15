5770f8a59513595be01406f8aea63877e86632e3
RoboRobert <nathan.estep@gmail.com>
2024-05-15 13:36:14 -0400

    adds chess pieces, removes some unneeded stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-  import svelteLogo from './assets/Chess_klt45.svg'
-  import viteLogo from './assets/Chess_kdt45.svg'
-  import Counter from './lib/Counter.svelte'
+  import whiteKing from './assets/Chess_klt45.svg'
+  import whiteQueen from './assets/Chess_qlt45.svg'
+  import whiteRook from './assets/Chess_rlt45.svg'
+  import whiteBishop from './assets/Chess_blt45.svg'
+  import whiteKnight from './assets/Chess_nlt45.svg'
+  import whitePawn from './assets/Chess_plt45.svg'
+
+  import blackKing from './assets/Chess_kdt45.svg'
+  import blackQueen from './assets/Chess_qdt45.svg'
+  import blackRook from './assets/Chess_rdt45.svg'
+  import blackBishop from './assets/Chess_bdt45.svg'
+  import blackKnight from './assets/Chess_ndt45.svg'
+  import blackPawn from './assets/Chess_pdt45.svg'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1>Hexagonal Chess</h1>
 
-  <div class="card">
-    <Counter />
+  <div>
+    <img src={whiteKing} class="piece white" alt="White king" />
+    <img src={whiteQueen} class="piece white" alt="White queen" />
+    <img src={whiteRook} class="piece white" alt="White rook" />
+    <img src={whiteBishop} class="piece white" alt="White bishop" />
+    <img src={whiteKnight} class="piece white" alt="White knight" />
+    <img src={whitePawn} class="piece white" alt="White pawn" />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div>
+    <img src={blackKing} class="piece black" alt="Black King" />
+    <img src={blackQueen} class="piece black" alt="Black King" />
+    <img src={blackRook} class="piece black" alt="Black King" />
+    <img src={blackBishop} class="piece black" alt="Black King" />
+    <img src={blackKnight} class="piece black" alt="Black King" />
+    <img src={blackPawn} class="piece black" alt="Black King" />
+  </div>
 </main>
 
 <style>
-  .logo {
+  .piece {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  .piece.black:hover {
+    filter: drop-shadow(0 0 2em #000000aa);
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .piece.white:hover {
+    filter: drop-shadow(0 0 2em #ffffffaa);
   }
 </style>
